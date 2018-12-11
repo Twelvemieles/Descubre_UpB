27c89e0b5496e34e382d68d6d168676bc6972084
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-12-11 18:41:59 -0500

    particulas balas

--- LoginFace/Assets/Survival/Juego/Script/BulletController.cs
+++ LoginFace/Assets/Survival/Juego/Script/BulletController.cs
@@ -7,9 +7,31 @@ public class BulletController : MonoBehaviour {
     [SerializeField]
     private int force;
 
-	void Update () {
+    private bool tocado;
+    private Rigidbody rb;
 
-        GetComponent<Rigidbody>().AddForce(transform.forward * force);
-
+    private void Start()
+    {
+        rb.GetComponent<Rigidbody>();
     }
+
+    void Update ()
+    {
+        rb.AddForce(transform.forward * force);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            Invoke("destruir", 1.5f);
+        }
+    }
+
+    public void destruir()
+    {
+        Destroy(this.gameObject);
+    }
+
 }
